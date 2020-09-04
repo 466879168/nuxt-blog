5e9aa69ae908b92973d8e6ec97b2f347d3f8386d
孟洋 <466879168@qq.com>
2020-09-04 13:43:30 +0800

    修改

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      {name:'baidu-site-verification',content:'6J9sOW5PbW'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/my-ico.ico' },
